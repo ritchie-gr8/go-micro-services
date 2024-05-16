d01c70ae3394b5ef0091c22f74a3aa68a965a57a
ritchie-gr8 <rit.yotsathon@gmail.com>
2024-05-16 17:59:55 +0700

    feat: add logger service handler in broker

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -11,11 +11,17 @@ type (
 	RequestPayload struct {
 		Action string      `json:"action"`
 		Auth   AuthPayload `json:"auth,omitempty"`
+		Log    LogPayload  `json:"log,omitempty"`
 	}
 
 	AuthPayload struct {
 		Email    string `json:"email"`
 		Password string `json:"password"`
+	}
+
+	LogPayload struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
 	}
 )
 
@@ -40,6 +46,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 	default:
 		app.errorJSON(w, errors.New("unknow action"))
 	}
@@ -47,11 +55,15 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	// create json that will be sent to the auth service
-	jsonData, _ := json.MarshalIndent(a, "", "\t")
+	jsonData, err := json.MarshalIndent(a, "", "\t")
+	if err != nil {
+		app.errorJSON(w, errors.New("cannot marshal the payload"), http.StatusBadRequest)
+	}
 
 	// call the service
+	authURL := "http://authentication-service/authenticate"
 	request, err := http.NewRequest("POST",
-		"http://authentication-service/authenticate",
+		authURL,
 		bytes.NewBuffer(jsonData),
 	)
 	if err != nil {
@@ -93,6 +105,46 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 		Error:   false,
 		Message: "Authenticated!",
 		Data:    jsonFromService.Data,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+// TODO: refactor a function
+// (maybe create a helper func to reduce boilerplate code when sending back a response)
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	jsonData, err := json.MarshalIndent(entry, "", "\t")
+	if err != nil {
+		app.errorJSON(w, errors.New("cannot marshal the payload"), http.StatusBadRequest)
+	}
+
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Logged!",
 	}
 
 	app.writeJSON(w, http.StatusAccepted, payload)
